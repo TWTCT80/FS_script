593ef4a11278ea91bc0be89e46627483a7126e04
twtct80 <tomthorsen@live.se>
2025-12-11 03:32:13 -0500

    Fixade scriptet och en output

--- o1/s2.sh
+++ o1/s2.sh
@@ -8,41 +8,49 @@
 # Last Update: 2025-01-01
 
 echo "Välkommen till mitt RECON script för att kontrollera en Linux-miljö"
-echo
+echo ""
 
 echo "=== SYSTEMINFO ==="
 uname -a
-echo
+echo ""
 
 echo "=== RAM INFO ==="
 free -h
-echo
+echo ""
+
+echo "=== DISKUTRYMME (Root) ==="
+df -h / | awk 'NR==2 {print "Använt: " $5 " | Ledigt: " $4}'
+echo ""
 
 echo "=== AKTUELL ANVÄNDARE ==="
 echo $USER
-echo
+echo ""
 
 echo "=== ANVÄNDARE MED SHELL ==="
 grep "sh$" /etc/passwd
-echo
+echo ""
 
 echo "=== NÄTVERK ==="
 ip a | grep inet
-echo
+echo ""
 
-echo "=Route="
+echo "=== PUBLIK IP ==="
+curl -s -m 5 ifconfig.me || echo "Ingen anslutning"
+echo ""
+echo "" 
+echo "=== IP Route ==="
 ip route
-echo
+echo ""
 
-echo "=Netstat="
+echo "=== Netstat ==="
 ss -tulnp
-echo
+echo ""
 
-echo "=== OS VERSION ==="
-cat /etc/os-release | grep PRETTY_NAME
-echo
+echo "=== UPPTID ==="
+uptime -p
+echo ""
+
 echo "=== TIME AND DATE ==="
 date
-
-echo
+echo ""
 
