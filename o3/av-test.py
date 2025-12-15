15f72716e2bd302787e4d798cc42cb1ea9ac4540
Tom Thorsén <tomthorsen@live.se>
2025-12-15 11:16:40 +0100

    Gjort klart script och lagt till printscreen

--- o3/av-test.py
+++ o3/av-test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import platform
+import time
+from pathlib import Path
 
 system = platform.system()
 
@@ -20,3 +22,20 @@ else:
     print(f"Okänt operativsystem {system}. Detta script är avsett för windows. Avbryter körning.")
     exit()
 
+
+eicar_str = r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+desktop = Path.home() / "Desktop"
+file_path = desktop / "test.txt"
+
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write(eicar_str)
+
+time.sleep(3)
+
+try:
+    with open(file_path, "r") as f:
+        content = f.read()
+
+except OSError as e:
+    print("Kunde inte läsa filen")
+    print("Ditt antivirus har troligtvis redan raderat filen")
